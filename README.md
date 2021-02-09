7fba4e97b3fd2f5ddc319e7a26116301642e2fd8
drewburr <drewburr9@gmail.com>
2021-02-08 23:11:22 -0600

    Add current files

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ For all numbers with 'x' digits, what percentage of them are in the first billio
 
 This repo is me attempting to build something that will answer this question in a reasonable amount of time.
 
+The first billion digits of pi are avilable through [MIT's site](https://stuff.mit.edu/afs/sipb/contrib/pi/#:~:text=One%20billion%20digits%20of%20%CF%80,in%20the%20file%20pi%2Dbillion.). Be sure to drop the decimal point for this to work correctly.
+
 ## External requirements
 
 This project uses a locally running Postgres server. I'm currently using a Postgres server running in a Docker container.
---
+++ interpolate.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+
+# import sys
+# print(sys.getsizeof(""))
+
+import psycopg2
+from multiprocessing import Pool
+
+SIZE = 5
+PROC_COUNT = 45
+
+
+def main():
+    test_db_connection()
+    initialize_db()
+
+    pi_data = split_data(get_pi_data(), PROC_COUNT)
+
+    with Pool(PROC_COUNT) as p:
+        p.map(process_data, pi_data)
+
+        # for segment in pi_data:
+        #     process_data(segment)
+
+
+"""
+Parsing and manipulation functions
+"""
+
+
+def get_pi_data():
+    # Get that data
+    f = open("pi-billion.txt", "r")
+    pi_data = f.read()
+    f.close()
+
+    return pi_data
+
+
+def split_data(arr, parts):
+    length = len(arr)
+    offset = SIZE-1
+    return [arr[i*length // parts: ((i+1)*length // parts)+offset] for i in range(parts)]
+
+
+def process_data(segment):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    subarr = list()
+
+    print("processing...")
+
+    for c in segment:
+        subarr.append(c)
+
+        # Skip publishing when still initializing
+        if len(subarr) == SIZE:
+            try:
+                cur.execute(
+                    f"INSERT INTO pi_values (number) VALUES ({''.join(subarr)})")
+                conn.commit()
+                subarr.pop(0)
+            except (Exception, psycopg2.errors.UniqueViolation):
+                pass
+
+    cur.close()
+    conn.close()
+
+
+"""
+SQLLite functions
+"""
+
+
+def connect_to_db():
+
+    dbhost = "localhost"
+    pguser = "localadmin"
+    pgpass = "X77i&%Hs$6JC*etr5F8!"
+    dbname = "test_db"
+    conn = psycopg2.connect(
+        host=dbhost, dbname=dbname, user=pguser, password=pgpass)
+
+    return conn
+
+
+def test_db_connection():
+
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    # Execute a test statement
+    # print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+    # print(cur.fetchone())
+    cur.close()
+
+
+def initialize_db():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    # cur.execute(
+    #     "CREATE TABLE IF NOT EXISTS pi_values (number TEXT UNIQUE, figures VARCHAR (50))")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS pi_values (number TEXT)")
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+main()
